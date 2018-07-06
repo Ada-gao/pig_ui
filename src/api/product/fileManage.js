39516eb5826dde0f493e430b89246993ce7794c1
gaoyuan <gaoyuan@shuyun365.com>
2018-07-06 18:15:40 +0800

    upload file

--- src/api/product/fileManage.js
+++ src/api/product/fileManage.js
@@ -1,13 +1,46 @@
 import request from '@/utils/request'
 
 /**
- * 查找上传客户材料
+ * 查找产品上传客户材料
  */
 export function getClientFile(query) {
   return request({
     url: '/product/productClientFileManage/',
     method: 'get',
     params: query
+  })
+}
+
+/**
+ * 新增产品上传客户材料
+ */
+export function addClientFile(obj) {
+  return request({
+    url: '/product/productClientFileManage/',
+    method: 'post',
+    data: obj
+  })
+}
+
+/**
+ * 修改产品上传客户材料
+ */
+export function updClientFile(id, obj) {
+  return request({
+    url: '/product/productClientFileManage/' + id,
+    method: 'put',
+    data: obj
+  })
+}
+
+/**
+ * 删除产品上传客户材料
+ */
+export function delClientFile(id, obj) {
+  return request({
+    url: '/product/productClientFileManage/' + id,
+    method: 'delete',
+    data: obj
   })
 }
 
@@ -19,5 +52,38 @@ export function getTranscFile(query) {
     url: '/product/productTransactionFileManage/',
     method: 'get',
     params: query
+  })
+}
+
+/**
+ * 新增交易所需材料
+ */
+export function addTranscFile(obj) {
+  return request({
+    url: '/product/productTransactionFileManage/',
+    method: 'post',
+    data: obj
+  })
+}
+
+/**
+ * 修改交易所需材料
+ */
+export function updTranscFile(id, obj) {
+  return request({
+    url: '/product/productTransactionFileManage/' + id,
+    method: 'put',
+    data: obj
+  })
+}
+
+/**
+ * 删除交易所需材料
+ */
+export function delTranscFile(id, obj) {
+  return request({
+    url: '/product/productTransactionFileManage/' + id,
+    method: 'delete',
+    data: obj
   })
 }
--- src/views/product/materialManagement/index.vue
+++ src/views/product/materialManagement/index.vue
@@ -1,22 +1,77 @@
 
 <template>
   <div class="app-container calendar-list-container">
-    
-    <product-search-component
-      :searchProductStatus="false"
-      @searchProduct="searchList">
-    </product-search-component>
 
-    <product-table-component>
-    </product-table-component>
-    
+    <div class="main-tab">
+      <el-radio-group v-model="step" @change="changeStep" style="margin-bottom: 30px;">
+        <el-radio-button label="1">交易所需材料</el-radio-button>
+        <el-radio-button label="2">上传客户材料</el-radio-button>
+      </el-radio-group>
+    </div>
+    <div style="text-align: right">
+      <!-- <el-button class="add_btn" @click="handleCreate">
+        <svg-icon icon-class="add"></svg-icon> 新增资料</el-button> -->
+      <el-upload
+        class="upload-demo"
+        style="display: inline-block;"
+        :headers="headers"
+        :action="importUrl"
+        :on-success="handleCreate"
+        :show-file-list="false"
+        accept=".pdf, .doc">
+        <el-button size="small"
+          class="btn-padding add_btn">新增材料</el-button>
+      </el-upload>
+    </div>
+    <el-table :key='tableKey' :data="list" v-loading="listLoading" element-loading-text="给我一点时间" border fit
+              highlight-current-row style="width: 100%">
+
+      <el-table-column align="center" label="材料名称">
+        <template slot-scope="scope">
+          <span>{{scope.row.name}}</span>
+        </template>
+      </el-table-column>
+
+      <el-table-column align="center" label="附件">
+        <template slot-scope="scope">
+          <span>{{scope.row.name}}</span>
+        </template>
+      </el-table-column>
+
+      <el-table-column align="center" label="文件大小">
+        <template slot-scope="scope">
+          <span>{{scope.row.fileSize}}</span>
+        </template>
+      </el-table-column>
+
+      <el-table-column align="center" label="操作" fixed="right" width="150">
+        <template slot-scope="scope">
+          <a size="small" class="common_btn"
+                     @click="handleRouter(scope.row.clientId)">编辑
+          </a>
+          <span class="space_line"> | </span>
+          <a size="small" class="common_btn"
+                     @click="handleUpdate(scope.row.clientId)">删除
+          </a>
+        </template>
+      </el-table-column>
+
+    </el-table>
+
+    <div v-show="!listLoading" class="pagination-container">
+      <el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange"
+                     :current-page.sync="listQuery.page"
+                     :page-sizes="[10,20,30, 50]" :page-size="listQuery.limit"
+                     layout="total, sizes, prev, pager, next, jumper" :total="total">
+      </el-pagination>
+    </div>
+
   </div>
 </template>
 
 <script>
-  import productSearchComponent from 'components/searchBar/product'
-  import productTableComponent from 'components/table/product'
-  import { fetchList, getObj, addObj, putObj, delObj } from '@/api/product/product'
+  import { getClientFile, addClientFile, updClientFile, delClientFile,
+    getTranscFile, addTranscFile, updTranscFile, delTranscFile } from '@/api/product/fileManage'
   import { fetchProductTypeList } from '@/api/product/productType'
   import { deptRoleList, fetchDeptTree } from '@/api/role'
   import { getFiles, delFiles, uploadFiles } from '@/api/qiniu'
@@ -33,9 +88,7 @@
   export default {
     components: {
       ElOption,
-      ElRadioGroup,
-      productSearchComponent,
-      productTableComponent
+      ElRadioGroup
     },
     name: 'table_user',
     directives: {
@@ -45,23 +98,14 @@
       return {
         treeDeptData: [],
         checkedKeys: [],
-        defaultProps: {
-          children: 'children',
-          label: 'name'
-        },
         list: null,
         total: null,
-        listLoading: true,
+        listLoading: false,
         listQuery: {
           page: 1,
           limit: 20,
-          // name: '',
-          productTypeIds: [],
-          productStatus: [],
-          annualizedReturns: [],
           isFloat: null
         },
-        role: undefined,
         form: {
           name: undefined,
           password: undefined,
@@ -110,7 +154,9 @@
         },
         isDisabled: true,
         form: [],
-        isSpread: false
+        isSpread: false,
+        step: 1,
+        importUrl: 'zuul/product/products/productTransactionFileManage/'
       }
     },
     computed: {
@@ -127,11 +173,15 @@
       this.sys_product_upd = this.permissions['sys_product_upd']
     },
     methods: {
+      changeStep(val) {
+        this.step = val
+        this.importUrl = 'zuul/product/products/productClientFileManage/'
+      },
       getList() {
         this.listLoading = true
         
         this.listQuery.isFloat ? this.listQuery.isFloat = 0: this.listQuery.isFloat = null
-        fetchList(this.listQuery).then(response => {
+        getTranscFile(this.listQuery).then(response => {
           this.list = response.data.records
           this.total = response.data.total
           this.listLoading = false
@@ -143,14 +193,9 @@
             })
           })
         })
-
         getObjList().then(response => {
           this.currencyList = response.data
         })
-      },
-      handleFilter() {
-        this.listQuery.page = 1
-        this.getList()
       },
       handleSizeChange(val) {
         this.listQuery.limit = val
@@ -161,13 +206,8 @@
         this.getList()
       },
       handleCreate() { //新增
-        this.$router.push({path: '/product/productDetail'})
-        Bus.$emit('activeIndex', '/product/productList')
+        
 
-        // this.resetTemp()
-        // this.dialogStatus = 'create'
-        // this.dialogFormVisible = true
-        // this.nextToUpdate = false
       },
       handleUpdate(row) { // 编辑
         this.$router.push({path: '/product/productDetail/' + row.productId})
@@ -176,29 +216,6 @@
         // this.nextToUpdate = false
       
       },
-      resetTemp() {
-        this.form = {
-          id: undefined,
-          name: '',
-          role: undefined
-        }
-      },
-      resetFilter() {
-        this.listQuery = {
-          name: '',
-          // type: [],
-          productTypeIds: [],
-          annualizedReturns: [],
-          productStatus: [],
-          isFloat: 0
-        }
-        this.getList()
-      },
-      searchList(data) {
-        this.listQuery = data
-        // this.listQuery.type = 1
-        this.getList()
-      }
     }
   }
 </script>
