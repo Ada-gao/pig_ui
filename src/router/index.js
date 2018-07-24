7e7c3483d63140660a7d30126973c375ac5cb215
许玉纯 <pure@xuyuchundeMacBook-Pro.local>
2018-07-24 14:20:51 +0800

    佣金列表

--- src/router/index.js
+++ src/router/index.js
@@ -229,17 +229,17 @@ export const asyncRouterMap = [
     menuId: 3,
     path: '/achievement',
     component: Layout,
-    name: '业绩/佣金管理',
+    name: '佣金管理',
     hidden: false,
     redirect: '/achievement/allList',
     // icon: 'table',
     meta: {
-      title: '业绩/佣金管理',
+      title: '佣金管理',
       icon: 'product_mg'
     },
     children: [
       { menuId: 48, path: 'balanced', component: _import('achievement/balanced/index'), name: '平衡计分卡', meta: { title: '平衡计分卡', icon: 'product' }},
-      { menuId: 48, path: 'list', component: _import('achievement/list/index'), name: '业绩/佣金列表', meta: { title: '业绩/佣金列表', icon: 'product' }},
+      { menuId: 48, path: 'list', component: _import('achievement/list/index'), name: '佣金列表', meta: { title: '佣金列表', icon: 'product' }},
       { menuId: 48, path: 'perform', component: _import('achievement/performance_indicator/index'), name: '业绩指标列表', meta: { title: '业绩指标列表', icon: 'product' }}
     ]
   },
