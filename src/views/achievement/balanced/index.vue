6c373aa4343fce783011f22ad56d8332240ba37b
许玉纯 <pure@xuyuchundeMacBook-Pro.local>
2018-08-07 16:12:41 +0800

    update

--- src/views/achievement/balanced/index.vue
+++ src/views/achievement/balanced/index.vue
@@ -194,11 +194,22 @@ export default {
 		blur() {
 			console.log('1111')
 		},
+		cycleList(list) {
+			list.forEach(item => {
+				if (item.children && !item.children.length) {
+					delete item.children
+				}
+				if (item.children && item.children.length) {
+					this.cycleList(item.children)
+				}
+			})
+		},
 		getList() {
 			this.listLoading = true
 			getAllDeparts()
 				.then(response => {
 					this.treeDeptData = response.data
+					this.cycleList(this.treeDeptData)
 			}),
 			getBalancedList(this.listQuery).then(response => {
 				this.list = response.data.records
