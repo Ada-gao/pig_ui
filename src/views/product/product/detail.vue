f8a2bb9ea69b109091c65abe4093d2b17b1c4795
gaoyuan <gaoyuan@shuyun365.com>
2018-09-12 15:08:50 +0800

    20 maxLength & role & productDetail valid

--- src/views/product/product/detail.vue
+++ src/views/product/product/detail.vue
@@ -10,7 +10,8 @@
       <el-radio-group v-model="step" @change="changeStep" style="margin-bottom: 30px;">
         <el-radio-button label="1">产品详情</el-radio-button>
         <el-radio-button style="border-radius: 0" label="2">产品操作指南</el-radio-button>
-        <el-radio-button v-if="productStatusNo!==0&&productStatusNo!==1&&stageType!='0'" label="3">交易信息</el-radio-button>
+        <el-radio-button v-if="productTran&&stageType!='0'" label="3">交易信息</el-radio-button>
+        <!-- <el-radio-button v-if="productStatusNo!==0&&productStatusNo!==1&&stageType!='0'" label="3">交易信息</el-radio-button> -->
       </el-radio-group>
     </div>
     <el-steps v-else :active="activeStep" simple finish-status="success">
@@ -216,11 +217,7 @@
         },
         keyProRules: {
           keyProduct: [
-            {
-              required: true,
-              trigger: 'blur, change',
-              message: '请标注产品'
-            }
+            { required: true, trigger: 'blur, change', message: '请标注产品' }
           ]
         },
         rules2: {
@@ -322,7 +319,8 @@
         productStatus: '',
         formData: {},
         activeStep: 0,
-        productInfo: {}
+        productInfo: {},
+        productTran: false // 控制交易tab
       }
     },
     computed: {
@@ -340,8 +338,12 @@
     created() {
       this.listQuery.productId = this.$route.params.id
       this.productId = this.$route.params.id
+      const proStatusText = this.$route.query.productStatus
+      // 获取状态速度更快，提升用户体验
+      if (proStatusText.indexOf('在建') === -1 && proStatusText.indexOf('预热') === -1) {
+        this.productTran = true
+      }
       if(this.productId) {
-        // console.log('this.createStatus')
         this.createStatus = 'update'
       }
       fetchProductTypeList().then(res => { // 获取产品类型
@@ -356,7 +358,6 @@
       this.sys_user_upd = this.permissions['sys_user_upd']
       this.sys_user_del = this.permissions['sys_user_del']
       this.activeStep = this.step - 1
-      // this.productStatusNo = this.productStatusNum
     },
     mounted() {
       Bus.$on('activeRouter', activeRouter => {
